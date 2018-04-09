04e2c1811b3fa91eac48833030a9b793334f1539
Vincenzo Ruffa <vincenzoruffa1991@gmail.com>
2018-04-09 15:57:03 +0200

    init plugin

---
+++ lib/Features/Microsoft.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: vincenzoruffa
+ * Date: 09/04/2018
+ * Time: 14:57
+ */
+
+namespace Features;
+
+
+use API\V2\Exceptions\AuthenticationError;
+use Features;
+use Klein\Klein;
+
+class Microsoft extends BaseFeature {
+
+    const FEATURE_CODE = "microsoft";
+
+    public static function loadRoutes( Klein $klein ) {
+
+
+    }
+}
