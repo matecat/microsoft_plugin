2459733de6449f2ebec3ecbfa2453099211b2da5
Vincenzo Ruffa <vincenzoruffa1991@gmail.com>
2018-05-24 17:01:35 +0200

    reviseQA custom

---
+++ lib/Features/Microsoft/Utils/Constants/Revise.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: vincenzoruffa
+ * Date: 23/05/2018
+ * Time: 18:20
+ */
+
+namespace Features\Microsoft\Utils\Constants;
+
+class Revise extends \Constants_Revise{
+
+    /**
+     * Max allowed errors for category
+     */
+    const WORD_INTERVAL    = 1000;
+    const MAX_TYPING       = 1;
+    const MAX_TRANSLATION  = 1;
+    const MAX_TERMINOLOGY  = 1;
+    const MAX_QUALITY      = 1;
+    const MAX_STYLE        = 5;
+
+
+}
